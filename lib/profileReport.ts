f3f455a4f90fe707df84f9315cfe76abacbb3420
mike dupont <mike.dupont@introspector.local>
2024-11-16 19:26:10 -0500

    WIP,
    now it is hanging somehow on the promise

--- lib/profileReport.ts
+++ lib/profileReport.ts
@@ -5,8 +5,16 @@ import 'server-only';
 //import AdmZip from 'adm-zip';
 import * as fs from 'fs';
 import * as unzip from 'unzip-stream';
-import * as tar from 'tar';
+
+import { createReadStream } from "fs";
+import { pipeline } from "stream";
+import { promisify } from "util";
+//import { gunzipMaybe } from "gunzip-maybe";
+import { extract } from 'tar-stream';
+
+
 import { fetchGitHubStream } from './github';
+import { resolve } from 'dns';
 
 // Represents the call frame for a node
 interface CallFrame {
@@ -48,16 +56,89 @@ async function unzipFile(zipFilePath: string, destPath: string): Promise<void> {
   });
 }
 
-// Function to untar the .tar file
-async function untarFile(tarFilePath: string, destPath: string): Promise<void> {
-  console.log("unTar ",tarFilePath,destPath);
-  //await tar.x({
-  //      file: tarFilePath,
-  //    C: destPath // Extract to the same directory
-  //  });
-  console.log("Tar extraction complete");
+// // Function to untar the .tar file
+// async function untarFile(tarFilePath: string, destPath: string): Promise<void> {
+//   console.log("unTar ",tarFilePath,destPath);
+//   // extracting a directory
+//   let res = fs.createReadStream(tarFilePath)//.pipe(gunzipMaybe()).pipe(extract(target));
+//   const extract2 = res.pipe(gunzipMaybe()).pipe(extract(destPath, { strip: 1 }));
+//   extract2.on('finish', () => {
+//     process.stdout.write(`Extracted to ${destPath}\n`);
+//     //resolve();
+//   });
+//   //extract2.on('error', reject);
+
+//   await extract2();
+//   console.log("Tar extraction complete");
+// }
+
+const pipelineAsync = promisify(pipeline);
+
+import gunzipMaybe from 'gunzip-maybe';
+import { Readable } from 'stream';
+import * as tarStream from 'tar-stream';
+
+export type TarballDetails = {
+  fileCount: number;
+  unpackedSize: number; // in bytes
+  parsedJson: any;// tree
+};
+// from github.com/verdaccio/versaccio
+export async function unpackTarball(tarFilepath: string, dest:string): Promise<TarballDetails> {
+  let fileCount = 0;
+  let unpackedSize = 0;
+  const readable = createReadStream(tarFilepath);
+  const unpack = tarStream.extract();
+
+  return new Promise((resolve, reject) => {
+    let parsedJson ={};
+
+    readable
+      .pipe(gunzipMaybe())
+      .pipe(unpack)
+      .on('entry', (header, stream, next) => {
+        fileCount++;
+        unpackedSize += Number(header.size);
+	if (header.name.endsWith(".cpuprofile")) {
+	  let data = '';
+	  const newname = dest + "/"+ header.name.replace("./profile/","");
+	  console.log("headername:",header.name,newname);
+	  const file = fs.createWriteStream(newname);
+	  stream.pipe(file); //on("data", datacall);
+          console.log("entry",fileCount, header);
+	}
+        stream.resume(); // important to ensure that "entry" events keep firing
+        next();
+      })
+      .on('finish', () => {
+	console.log("finishe");
+        resolve({
+          fileCount,
+          unpackedSize,
+	  parsedJson,
+        });
+      })
+      .on('error', reject);
+  });
 }
 
+async function untarFile(tarFilePath: string, destPath: string): Promise<void> {
+  console.log("unTar", tarFilePath, destPath);
+  await unpackTarball(tarFilePath, destPath);
+  console.log("after unTar");
+  // try {
+  //   // Create read stream from tar file and process with gunzip and extract
+  //   await pipelineAsync(
+  //     createReadStream(tarFilePath),
+  //     gunzipMaybe(),
+  //     extract( {  })
+  //   );
+  //   console.log(`Extracted to ${destPath}`);
+  // } catch (error) {
+  //   console.error("Error extracting tar file:", error);
+  //   throw error; // Rethrow error for caller to handle
+  // }
+}
 
 // Function to process the JSON files
 async function processJsonFiles(directory: string): Promise<any[]> {
@@ -98,7 +179,7 @@ export async function readZip(path: string, filename:string) {
   }
   const profilepath = path + "/profile";
   if (!fs.existsSync(profilepath)) {
-    //    await untarFile(tarFilePath, path);
+    await untarFile(tarFilePath, path);
   }
   else {
     console.log("tar already unpacked");
